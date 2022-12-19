7c06ac7bbc1a985bb7ab1449d5944918d9014e48
Marc McAdoo <77635027+mcadoo300@users.noreply.github.com>
2022-12-19 09:38:25 -0500

    Update reciprocal_cycles.py

--- reciprocal_cycles.py
+++ reciprocal_cycles.py
@@ -1,3 +1,19 @@
+#problem 26
+#A unit fraction contains 1 in the numerator. The decimal representation of the unit fractions with denominators 2 to 10 are given:
+#1/2	= 	0.5
+#1/3	= 	0.(3)
+#1/4	= 	0.25
+#1/5	= 	0.2
+#1/6	= 	0.1(6)
+#1/7	= 	0.(142857)
+#1/8	= 	0.125
+#1/9	= 	0.(1)
+#1/10	= 	0.1
+#Where 0.1(6) means 0.166666..., and has a 1-digit recurring cycle. It can be seen that 1/7 has a 6-digit recurring cycle.
+#Find the value of d < 1000 for which 1/d contains the longest recurring cycle in its decimal fraction part.
+
+
+
 #will return the repeating sequence of a digit 1/x
 #if 1/x is not repeating this function return -1
 def get_sequence(x):
@@ -24,8 +40,8 @@ while not flag:
         maxRep = get_sequence(n)
         #for each n there are n-1 possible remainders(if the remainder is 0 its not repeating)
         #so when the sequence is larger than the next element in the loop we have our max repeating decimal
-    if maxRep >= (n-1):
-        flag = True
+        if maxRep >= (n-1):
+            flag = True
 
     n -= 1
 
